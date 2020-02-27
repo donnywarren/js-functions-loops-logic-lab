bcf9d9013564c716bdf8c137882e802546d11209
donnywarren <51206190+donnywarren@users.noreply.github.com>
2020-02-27 17:21:12 -0500

    alerts set

---
+++ js-practice/calculator.js
@@ -0,0 +1,21 @@
+function add(num1, num2) {
+  let total = num1 + num2;
+  alert(`${num1} + ${num2} = ${total}`);
+}
+
+function subtract(num1, num2) {
+  let total = num1 - num2;
+  alert(`${num1} - ${num2} = ${total}`);
+}
+
+function multiply(num1, num2) {
+  let total = num1 * num2;
+  alert(`${num1} * ${num2} = ${total}`);
+}
+
+function divide(num1, num2) {
+  let total = num1 / num2;
+  alert(`${num1} / ${num2} = ${total}`);
+}
+
+console.log(divide(4, 5));
